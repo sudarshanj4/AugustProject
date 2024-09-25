80640a3ccdd6caaa4d23f22a673ffbca5215d157
vijaym2211 <86069882+vijaym2211@users.noreply.github.com>
2024-09-25 20:17:13 +0530

    Update SudarshanProjectSpringbootApplication.java

--- src/main/java/com/example/sudarshan_projectspringboot/SudarshanProjectSpringbootApplication.java
+++ src/main/java/com/example/sudarshan_projectspringboot/SudarshanProjectSpringbootApplication.java
@@ -9,5 +9,20 @@ public class SudarshanProjectSpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SudarshanProjectSpringbootApplication.class, args);
     }
+        public Product updateById(long id, String name, String category, String description) throws ProductNotFoundException{
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isEmpty()) {
+            throw new ProductNotFoundException("Product with id: " + id + " not found.");
+        }
+
+        Product p = optionalProduct.get();  // Get the actual Product from the Optional
+
+        p.setCategory(category);
+        p.setDescription(description);
+        p.setName(name);
+        p = productRepository.save(p);
+        System.out.println(p.getId());
+        return p;
+    }
 
 }
